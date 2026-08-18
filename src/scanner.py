f40b6e1f2ee0ee73a615793f9c6cc32519547396
wagrude <shivansh.work.college@gmail.com>
2026-08-18 22:22:26 +0530

    scanner edits

--- src/scanner.py
+++ src/scanner.py
@@ -1,4 +1,30 @@
 import os
 
+IGNORED_DIRS = {".git", ".venv", "__pycache__"}
+
+SUPPORTED_EXTENSIONS = {
+    ".c",
+    ".h",
+    ".cpp",
+    ".hpp",
+    ".py",
+    ".js",
+    ".ts",
+    ".java"
+}
+
 def scan_project(path):
-    pass
+    files_found = []
+
+    for root, dirs, files in os.walk(path):
+        dirs[:] = [directory for directory in dirs if directory not in IGNORED_DIRS]
+
+        for file in files:
+            file_path = os.path.join(root, file)
+            files_found.append(file_path)
+
+    return files_found
+
+
+files = scan_project(".")
+print(files)
